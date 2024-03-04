74a8bd40a083dc6aacc55001cb7ef83000cdccda
sevelinCa <ngabosevelin@gmail.com>
2024-03-04 02:18:09 +0200

    adding javascript function on navbar menu and testimoanial slide

---
+++ main.js
@@ -0,0 +1,47 @@
+const navbar = document.getElementById('navbarphone');
+const toggle = document.getElementById('toggle');
+
+toggle.addEventListener('click', (e) => {
+    navbar.classList.toggle('active');
+});
+
+const testimonials = document.querySelectorAll('.test-card');
+const prev = document.querySelector('.prev')
+const next = document.querySelector('.next')
+let startIndex = 0
+
+function showTestimonials(activeIndex) {
+    testimonials.forEach((item,index)=>{
+        item.style.display= "none"
+    })
+
+    for(let i = activeIndex;i<activeIndex+2;i++){
+        testimonials[i].style.display = 'block'
+
+    }
+    
+
+}
+showTestimonials(0);
+
+next.addEventListener('click',()=>{
+    if(startIndex + 2 > testimonials.length -1){
+        
+
+    }else{
+
+        startIndex = startIndex+2
+        showTestimonials(startIndex)
+    }
+
+})
+prev.addEventListener('click',()=>{
+    if(startIndex - 2 < 0 ){
+
+    }else{
+
+        startIndex = startIndex-2
+        showTestimonials(startIndex)
+    }
+
+})
